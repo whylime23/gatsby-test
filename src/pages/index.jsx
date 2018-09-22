223384d7bda8648438f4e34e0eed0479f0cdf986
Emily Taylor <emilytaylor23@outlook.com>
2018-09-22 16:25:14 +1000

    Challenge 2 commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,53 +1,52 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { graphql } from "gatsby";
+import axios from "axios";
 import Layout from "../layout";
 import PostListing from "../components/PostListing";
 import SEO from "../components/SEO";
 import config from "../../data/SiteConfig";
 
+// Replaced GraphQL query with Axios GET request and saved response in state.
+// References:
+// https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2
+// https://www.npmjs.com/package/axios
+
 class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      apiData: null
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://9ss7bxey8k.execute-api.ap-southeast-2.amazonaws.com/default/dummy_service')
+      .then((response) => {
+        this.setState({apiData: response.data.Data});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
-    return (
-      <Layout location={this.props.location} title="Home">
-        <div className="index-container">
-          <Helmet>
-            <title>{config.siteTitle}</title>
-            <link rel="canonical" href={`${config.siteUrl}`} />
-          </Helmet>
-          <SEO postEdges={postEdges} />
-          <PostListing postEdges={postEdges} />
-        </div>
-      </Layout>
-    );
+    if (this.state.apiData) {
+      const postEdges = this.state.apiData;
+      return (
+        <Layout location={this.props.location} title="Home">
+          <div className="index-container">
+            <Helmet>
+              <title>{config.siteTitle}</title>
+              <link rel="canonical" href={`${config.siteUrl}`} />
+            </Helmet>
+            <SEO postEdges={postEdges} />
+            <PostListing postEdges={postEdges} />
+          </div>
+        </Layout>
+      );
+    }
+    return null;
   }
 }
 
 export default Index;
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [fields___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
-`;
